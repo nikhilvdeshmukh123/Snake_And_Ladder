ac886266625dea73cff8aec85688ed7f35c34123
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-06-23 00:50:38 +0530

    Added: Player Rolls the Die to get number between 1 to 6

--- src/main/java/com/bridgelabz/SnakeAndLadder.java
+++ src/main/java/com/bridgelabz/SnakeAndLadder.java
@@ -1,7 +1,8 @@
 package com.bridgelabz;
 /*
  * @author : Nikhil Deshmukh
- * Snake and Ladder game played with single player at start position 0
+ * The Player rolls the die to get a number between 1 to 6.
+ *  - Use ((RANDOM)) to get the number between 1 to 6
  */
 public class SnakeAndLadder {
     // Declaring Instance Variable
@@ -12,11 +13,20 @@ public class SnakeAndLadder {
         System.out.println("Player Position: " + playerPosition);
     }
 
+    // Method to Roll the die and get no between 1 to 6
+    public int rollDie() {
+        int dieNo = (int) (Math.floor(Math.random() * 10) % 6 + 1);
+        return dieNo;
+    }
+
     public static void main(String[] args) {
         // Displaying welcome message
         System.out.println("Welcome to Snake and Ladder Simulation");
         // Creating object for Player
         SnakeAndLadder player = new SnakeAndLadder();
+        // Showing player position
         player.showPosition();
+        // Showing Die Rolling number
+        System.out.println("Die Number for Player: " + player.rollDie());
     }
 }
